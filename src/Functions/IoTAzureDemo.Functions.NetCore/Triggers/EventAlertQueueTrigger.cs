6a583cf0f6f71a65677461e7cf62553852ad86fc
gjlumsden <gjlumsden@gmail.com>
2020-01-20 11:23:05 +0000

    Updated Functions to V2. Updated all packages.

---
+++ src/Functions/IoTAzureDemo.Functions.NetCore/Triggers/EventAlertQueueTrigger.cs
@@ -0,0 +1,91 @@
+﻿using IotAzureDemo.Model;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IotAzureDemo.Functions.Triggers
+{
+    public static class EventAlertQueueTrigger
+    {
+        private static string alertSenderName = Environment.GetEnvironmentVariable(nameof(alertSenderName));
+        private static string alertSenderAddress = Environment.GetEnvironmentVariable(nameof(alertSenderAddress));
+
+        [FunctionName("EventAlertFunction")]
+        public static async Task Run([QueueTrigger("%alertsQueueName%", Connection ="alertStorageQueueConnection")]string message, 
+            [SendGrid(ApiKey = "sendgridKey")]IAsyncCollector<SendGridMessage> mail, ILogger log)
+        {
+            try
+            {
+                var evt = JsonConvert.DeserializeObject<EventBase>(message);
+                var recipients = Environment.GetEnvironmentVariable("emailAlertRecipients").Split(new[] { ',' },
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (evt.EventType.ToString().Equals("maintenance", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var maintenanceEvent = JsonConvert.DeserializeObject<MaintenanceEvent>(message);
+
+                    var emails =
+                        recipients.Select(
+                            x =>
+                            {
+                                var sgMessage = new SendGridMessage
+                                {
+                                    From = new EmailAddress(alertSenderAddress, alertSenderName),
+                                    Subject = $"Maintenance Alert: {maintenanceEvent.DeviceName} - {maintenanceEvent.Action}"
+                                };
+                                sgMessage.AddTo(new EmailAddress(x));
+                                sgMessage.AddContent("text/plain", GetMaintenanceEventEmailContent(maintenanceEvent));
+                                return sgMessage;
+                            });
+                    foreach (var e in emails)
+                    {
+                        await mail.AddAsync(e);
+                    }
+                }
+                else
+                {
+                    var thresholdEvent = JsonConvert.DeserializeObject<ThresholdEvent>(message);
+                    var emails =
+                        recipients.Select(
+                            x =>
+                            {
+                                var sgMessage = new SendGridMessage();
+                                sgMessage.From = new EmailAddress(alertSenderAddress, alertSenderName);
+                                sgMessage.Subject = $"Threshold Alert: {thresholdEvent.DeviceName} - {thresholdEvent.EventType}";
+                                sgMessage.AddTo(new EmailAddress(x));
+                                sgMessage.AddContent("text/plain", GetThresholdEventEmailContent(thresholdEvent));
+                                return sgMessage;
+                            });
+                    foreach (var e in emails)
+                    {
+                        await mail.AddAsync(e);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                log.LogError(0, ex, "Failed to send alerts.");
+            }
+        }
+
+        private static string GetThresholdEventEmailContent(ThresholdEvent thresholdEvent)
+        {
+            return $"{thresholdEvent.Severity} threshold crossed on detected Device {thresholdEvent.DeviceName}." +
+                   Environment.NewLine +
+                   $"{thresholdEvent.EventType} crossed the event threshold at {thresholdEvent.EventTime:yyyy-MM-ddTHH:mm:ssZ}." +
+                   Environment.NewLine +
+                   $"The value when the threshold was crossed was {thresholdEvent.CurrentValue}." + Environment.NewLine +
+                   $"This value was observed for {thresholdEvent.DurationSeconds} seconds before the event was raised.";
+        }
+
+        private static string GetMaintenanceEventEmailContent(MaintenanceEvent maintenanceEvent)
+        {
+            return
+                $"A {maintenanceEvent.Action} maintenance action was {(maintenanceEvent.Succeeded ? "successfully" : "not successfully")} performed at {maintenanceEvent.EventTime:yyyy-MM-ddTHH:mm:ssZ} on the equipment monitored by device {maintenanceEvent.DeviceName}.";
+        }
+    }
+}
---
+++ src/Functions/IoTAzureDemo.Functions.NetCore/Triggers/SensorEventMessageTrigger.cs
@@ -0,0 +1,103 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.Devices;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using IotAzureDemo.Model;
+using Microsoft.Extensions.Logging;
+
+namespace IotAzureDemo.Functions.Triggers
+{
+    public static class SensorEventMessageTrigger
+    {
+        static readonly ServiceClient Client = ServiceClient.CreateFromConnectionString(Environment.GetEnvironmentVariable("DeviceManagementConnection"));
+        static readonly RegistryManager Registry = RegistryManager.CreateFromConnectionString(Environment.GetEnvironmentVariable("DeviceManagementConnection"));
+        static bool emailEnabled = bool.Parse(Environment.GetEnvironmentVariable("EmailEnabled") ?? "false");
+
+        [FunctionName("SensorEventFunction")]
+        public static async Task Run([EventHubTrigger("telemetry", Connection = "sensorEventConnectionString", ConsumerGroup = "%eventsConsumerGroup%")]string message, 
+            [Queue("%alertsQueueName%", Connection ="alertStorageQueueConnection")]IAsyncCollector<EventBase> events,
+            [CosmosDB("IotDemoDb","Events",CollectionThroughput = 400,ConnectionStringSetting = "cosmosDbConnectionString",CreateIfNotExists =true, PartitionKey = "/DeviceId")]IAsyncCollector<EventBase> cosmosEvents,
+            ILogger log, ExecutionContext context)
+        {
+            log.LogInformation(message);
+            var evts = JsonConvert.DeserializeObject<AsaThresholdEvent[]>(message); //deserialise as an AsaThresholdEvent. This accounts for the nasty casing on json properties.
+            foreach (var evt in evts.Select(x=>x.AsThresholdEvent()))
+            {
+                if (emailEnabled)
+                {
+                    await events.AddAsync((ThresholdEvent)evt); //Pass through to alerts.
+                }
+                await cosmosEvents.AddAsync((ThresholdEvent)evt); //Pass Through to CosmosDB
+                if (evt.Severity.Equals("critical", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    await PerformMaintenanceAction("running", "Shutdown", evt, log, events, cosmosEvents);
+                }
+                if (evt.Severity.Equals("healthy", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    await PerformMaintenanceAction("shutdown", "Start", evt, log, events, cosmosEvents);
+                }
+            }
+        }
+
+        private static async Task PerformMaintenanceAction(string statusConditionValue, string action, EventBase triggerEvent, ILogger log, IAsyncCollector<EventBase> events, IAsyncCollector<EventBase> cosmosEvents)
+        {
+            var status = await GetStatusFromTwin(triggerEvent.DeviceId, log);
+            if ((status ?? string.Empty).Equals(statusConditionValue, StringComparison.InvariantCultureIgnoreCase) || string.IsNullOrWhiteSpace(status))
+            {
+                var result = await CallDirectMethod(log, triggerEvent.DeviceId, action.ToLower());
+                var evt = new MaintenanceEvent
+                {
+                    EventType = "Maintenance",
+                    Action = action,
+                    Succeeded = result,
+                    DeviceId = triggerEvent.DeviceId,
+                    DeviceName = triggerEvent.DeviceName,
+                    EventTime = DateTime.UtcNow
+                };
+                await cosmosEvents.AddAsync(evt);
+                if (emailEnabled)
+                {
+                    await events.AddAsync(evt);
+                }
+            }
+        }
+
+        private static async Task<string> GetStatusFromTwin(string deviceId, ILogger log)
+        {
+            try
+            {
+                var twin = await Registry.GetTwinAsync(deviceId);
+                var status = twin.Properties.Reported.Contains("EquipmentStatus")
+                    ? twin.Properties.Reported["EquipmentStatus"].ToString()
+                    : null;
+                return status;
+            }
+            catch (Exception ex)
+            {
+                log.LogError(0, ex, "Failed to retrieve status.");
+                return null;
+            }
+        }
+
+        private static async Task<bool> CallDirectMethod(ILogger log, string deviceId, string methodName)
+        {
+            await Client.OpenAsync();
+            var method = new CloudToDeviceMethod(methodName)
+            {
+                ResponseTimeout = TimeSpan.FromSeconds(30)
+            };
+            try
+            {
+                var result = await Client.InvokeDeviceMethodAsync(deviceId, method);
+                return result.Status == 200;
+            }
+            catch (Exception ex)
+            {
+                log.LogError(0,ex,"Failed to call device method.");
+                return false;
+            }
+        }
+    }
+}
